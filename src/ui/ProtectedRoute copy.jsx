ddaac7ff2a924ce403cd534073a6f217ef76fd49
rajib18197 <rajib18197@gmail.com>
2024-03-24 22:36:49 +0600

    added a protected route

--- src/ui/ProtectedRoute copy.jsx
+++ src/ui/ProtectedRoute copy.jsx
@@ -4,7 +4,6 @@ import { useAuth } from "../contexts/AuthContext";
 export default function ProtectedRoute({ children }) {
   // 1) load the authenticated user
   const { authData } = useAuth();
-  console.log(!authData.user);
 
   // 3) If Not authenticated, navigate to login page
   if (!authData?.user) return <Navigate to={"/login"} />;
