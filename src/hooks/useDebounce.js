1ac3e92363e07526dd1a101f5bc84d6d3674fe6a
rajib18197 <rajib18197@gmail.com>
2024-03-24 22:33:17 +0600

    implement useDebounce custom hook

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,10 +1,32 @@
-let num = -90;
-let n = num < 0 ? num * -1 : num;
-let rev = 0;
-while (n !== 0) {
-  let rem = n % 10;
-  rev = rev * 10 + rem;
-  n = Math.floor(n / 10);
+import { useEffect, useRef } from "react";
+
+export function useDebounce(callback, delay) {
+  const ref = useRef();
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(ref.current);
+    };
+  }, []);
+
+  function doSearch(...args) {
+    if (ref.current) clearTimeout(ref.current);
+
+    ref.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  }
+
+  return { doSearch };
 }
-rev = num < 0 ? -rev : rev;
-console.log(rev);
+
+// let num = -90;
+// let n = num < 0 ? num * -1 : num;
+// let rev = 0;
+// while (n !== 0) {
+//   let rem = n % 10;
+//   rev = rev * 10 + rem;
+//   n = Math.floor(n / 10);
+// }
+// rev = num < 0 ? -rev : rev;
+// console.log(rev);
