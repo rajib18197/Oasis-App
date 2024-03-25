60cdda9ab4cb1f102610fcd4dbe7b2e10d2960f8
rajib18197 <rajib18197@gmail.com>
2024-03-25 20:46:31 +0600

    added useInfinteQuery custom hook

---
+++ src/hooks/useInfiniteQuery.js
@@ -0,0 +1,33 @@
+import { useCallback, useLayoutEffect, useRef } from "react";
+
+export function useInfiniteQuery({ hasNextPage, callback }) {
+  let intObserver = useRef();
+  const ref = useRef(callback);
+
+  // Same pattern here as did in the useUsersData hook
+  useLayoutEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  const refCallback = useCallback(
+    function (node) {
+      console.log(node);
+      if (intObserver.current) intObserver.current.disconnect();
+
+      if (node) {
+        intObserver.current = new IntersectionObserver(([entry]) => {
+          if (entry.isIntersecting) {
+            if (hasNextPage) {
+              ref.current((page) => page + 1);
+            }
+          }
+        });
+
+        intObserver.current.observe(node);
+      }
+    },
+    [hasNextPage]
+  );
+
+  return { refCallback };
+}
